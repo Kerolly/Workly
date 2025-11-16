b5d467ac99da07c54b5f726f956714da747964e0
Andrei Ticarat <ticarat.andrei125@gmail.com>
2025-11-16 21:42:12 +0200

    Add: Header, time start/end, date, total hours

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -1,15 +1,11 @@
 // EmployeeDashboard.jsx
 
 import Header from "@/pages/components/Header.jsx"
-import {
-    Box,
-    Button,
-    Input,
-    Text,
-    HStack, Flex, Link, Alert
-} from "@chakra-ui/react";
-import {useState} from "react";
+import "@/styles/theme.css";
+import {Box, Field, Input, InputGroup, Stack, Text} from "@chakra-ui/react";
 import {useParams} from "react-router-dom";
+import {useRef} from "react";
+import {Calendar, Plus, Clock, Clock7} from "lucide-react"
 
 
 export default function EmployeeDashboard() {
@@ -17,11 +13,111 @@ export default function EmployeeDashboard() {
 
     const employeeId = useParams().employeeId;
 
+    const dateInputRef = useRef(null)
+    const startTimeInputRef = useRef(null)
+    const endTimeInputRef = useRef(null)
+
+    const handleOpenPicker = (inputRef) => {
+        if (inputRef.current) {
+            inputRef.current.showPicker()
+        }
+    }
+
+
+
     return (
         // Header
         <>
-        <Header />
-        <Text>Here is the employeeId: {employeeId}</Text>
+            <Header/>
+
+            <Box bg={"var(--background)"} minH="100vh" px={["20px", "60px"]}
+                 display={"flex"} flexDirection={"column"} alignItems={"center"}>
+
+                <Box maxW={"1040px"} w={"100%"}>
+
+                    {/*--- Welcome Section ---*/}
+                    <Box color={"var(--black)"} pt={["40px", "60px"]}>
+                        <Text textAlign={"start"} textStyle={["2xl", "3xl"]} fontWeight={"bold"}>Welcome, Andrei</Text>
+                        <Text textAlign={"start"} textStyle={["sm", "md"]} mt={"5px"}
+                              color={"var(--grey-600)"}>Manage your timesheets and view salary information</Text>
+                    </Box>
+
+                    {/*--- Work Time Registration Section ---*/}
+                    <Box color={"var(--black)"} bg={"var(--white)"} rounded={"lg"} mt={["25px", "35px"]}>
+                        <Box width={"100%"} p={["10px", "20px"]}>
+
+                            <Text textStyle={["lg", "xl"]}
+                                  display={"flex"} gap={"1"} alignItems={"center"}><Plus color={"var(--primary)"}/>Record
+                                worked hours</Text>
+
+                            <Stack direction={{base: "column", md: "row"}} gap={10} mt={"30px"}>
+
+                                {/*--- Time Start-End Section ---*/}
+                                <Field.Root required color={"var(--black)"}>
+                                    <Field.Label>Time Start<Field.RequiredIndicator/></Field.Label>
+                                    <InputGroup endElement={<Clock onClick={() => handleOpenPicker(startTimeInputRef)}
+                                                                     color="var(--primary)" cursor={"pointer"}/>}>
+                                        <Input type={"time"} ref={startTimeInputRef}/>
+                                    </InputGroup>
+                                </Field.Root>
+
+
+                                <Field.Root required color={"var(--black)"}>
+                                    <Field.Label>Time End<Field.RequiredIndicator/></Field.Label>
+                                    <InputGroup endElement={<Clock7 onClick={() => handleOpenPicker(endTimeInputRef)}
+                                                                   color="var(--primary)" cursor={"pointer"}/>}>
+                                        <Input type={"time"} ref={endTimeInputRef}/>
+                                    </InputGroup>
+                                </Field.Root>
+
+                            </Stack>
+
+                            {/*--- Date-Hour Section ---*/}
+                            <Stack direction={{base: "column", md: "row"}} gap={10} mt={"30px"}>
+
+                                <Field.Root required color={"var(--black)"}>
+                                    <Field.Label>Date <Field.RequiredIndicator/></Field.Label>
+                                    <InputGroup endElement={<Calendar onClick={() => handleOpenPicker(dateInputRef)}
+                                                                      color="var(--primary)" cursor={"pointer"}/>}>
+                                        <Input type={"date"} ref={dateInputRef}/>
+                                    </InputGroup>
+                                </Field.Root>
+
+                                <Field.Root color={"var(--black)"}>
+                                    <Field.Label>Total Hours</Field.Label>
+                                    <InputGroup endElement={<Clock color="var(--primary)"/>}>
+                                        <Input disabled type={"text"}/>
+                                    </InputGroup>
+                                </Field.Root>
+
+                            </Stack>
+
+                            {/*--- Job-Pay Section ---*/}
+                            <Stack direction={{base: "column", md: "row"}} gap={10} mt={"30px"}>
+
+                                <Field.Root required color={"var(--black)"}>
+                                    <Field.Label>Activity <Field.RequiredIndicator/></Field.Label>
+                                    <InputGroup endElement={<Calendar onClick={() => handleOpenPicker(dateInputRef)}
+                                                                      color="var(--primary)" cursor={"pointer"}/>}>
+                                        <Input type={"date"} ref={dateInputRef}/>
+                                    </InputGroup>
+                                </Field.Root>
+
+                                <Field.Root color={"var(--black)"}>
+                                    <Field.Label>Hourly Rates</Field.Label>
+                                    <InputGroup endElement={<Clock color="var(--primary)"/>}>
+                                        <Input disabled type={"text"}/>
+                                    </InputGroup>
+                                </Field.Root>
+
+                            </Stack>
+
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+
+
         </>
     )
 
--- frontend/src/pages/components/Header.jsx
+++ frontend/src/pages/components/Header.jsx
@@ -1,6 +1,7 @@
 // Header.jsx
 
 import worklyIcon from "@/assets/workly.svg";
+import "@/styles/theme.css";
 import {
     Box,
     Button,
@@ -8,13 +9,37 @@ import {
     Text,
     HStack, Flex, Link, Alert
 } from "@chakra-ui/react";
+import {logout} from "@/authHandler.js";
+import {useNavigate} from "react-router";
+import {LogOut} from "lucide-react"
 
 export default function Header(){
 
+    // Navigator
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout() // call logout
+        navigate("/login")
+    }
+
     return (
-        <Flex direction="row" justify="space-between">
-            <Text>asdasd</Text>
-            <Text>asdasd</Text>
+        <Flex direction="row" justify="space-between" alignItems={"center"} px={"50px"} height={"70px"} bg={"var(--primary-50)"}
+              shadow={"1px 1px 3px 0 rgba(0, 0, 0, 1)"}>
+
+            {/*--- Logo Section ---*/}
+            <Box gap={"4"} display={"flex"} alignItems={"center"}>
+                <img src={worklyIcon} alt="workly" width="12%" height="12%"/>
+                <Text color={"var(--black)"} textStyle={["lg", "2xl"]} fontWeight="bold">Workly</Text>
+            </Box>
+
+            {/*--- Menu Section ---*/}
+            <Box>
+                <Button variant="plain" bg={{ base: "transparent", _hover: "var(--primary)" }}
+                        color={{ base: "var(--black)", _hover: "var(--primary-50)" }}
+                        onClick={handleLogout}><LogOut/> Logout</Button>
+            </Box>
+
         </Flex>
     )
 
