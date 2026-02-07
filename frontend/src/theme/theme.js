e7faaa6d4d627a20ea182561870090e240312dc7
Andrei Ticarat <ticarat.andrei125@gmail.com>
2026-02-07 19:58:49 +0200

    Add: custom Chakra UI system theme and integrate with provider

---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,48 @@
+// theme.js
+
+import { createSystem, defaultConfig } from "@chakra-ui/react"
+
+const systemTheme = createSystem(defaultConfig, {
+    theme:{
+        tokens: {
+            colors: {
+                primary : {
+                    // Brand color: #f77518
+
+                    100: { value: '#feecd9' },
+                    200: { value: '#fddab0' },
+                    300: { value: '#fcbf80' },
+                    400: { value: '#fa9e4d' },
+                    500: { value: '#F77518' }, // Principal color
+                    600: { value: '#cc5d10' }, // Hover State
+                    700: { value: '#99430a' },
+                    800: { value: '#662c05' },
+                    900: { value: '#331502' },
+                },
+
+                secondary: {
+                    50: { value: '#f8fafc' },
+                    100: { value: '#f1f5f9' }, // Input disable
+                    200: { value: '#e2e8f0' }, // Subtle borders
+                    300: { value: '#cbd5e1' }, // Inactive icons
+                    400: { value: '#94a3b8' }, // Secondary text
+                    500: { value: '#64748b' },
+                    600: { value: '#475569' },
+                    700: { value: '#334155' },
+                    800: { value: '#1E293B' }, // Secondary color
+                    900: { value: '#0f172a' }, // Text
+                }
+            },
+        }
+    },
+
+    globalCss: {
+        "html, body": {
+            bg: "primary.50",
+            color: "secondary.900",
+            fontFamily: "Inter, sans-serif",
+        }
+    }
+})
+
+export default systemTheme
