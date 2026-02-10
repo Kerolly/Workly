0eb921378c4893ea9aa91333a79f2885766afa56
Andrei Ticarat <ticarat.andrei125@gmail.com>
2026-02-10 21:47:15 +0200

    Add: PersonalInfoCard to ProfilePage and restructure layout with Grid

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -2,18 +2,26 @@
 
 import {Box, Flex, Grid, GridItem, Heading, Text} from "@chakra-ui/react";
 import SidebarComponent from "@/components/layout/SidebarComponent.jsx";
+import PersonalInfoCard from "@/components/layout/PersonalInfoCard.jsx";
 
 export default function ProfilePage() {
     return (
         <>
             <Flex direction={"row"}>
                 <SidebarComponent/>
-                <Box>
+                <Grid templateColumns={{base: "1fr", lg: "3fr 1fr"}} >
 
-                    <Heading>My Profile</Heading>
-                    <Text>Find your personal information and activities rate</Text>
+                    <Box bg={"aqua"}>
+                        <PersonalInfoCard/>
 
-                </Box>
+                    </Box>
+                    <Box bg={"black"}>
+                        <Text>Find your personal information and activities rate</Text>
+                    </Box>
+
+
+
+                </Grid>
             </Flex>
 
         </>
