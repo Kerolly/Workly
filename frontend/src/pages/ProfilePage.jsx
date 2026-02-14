d978341760e2417613e51c0349c43de5942b3e8a
Andrei Ticarat <ticarat.andrei125@gmail.com>
2026-02-14 21:31:10 +0200

    Enhance: restructure ProfilePage layout, add InfoCard for activities, and improve spacing

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,24 +1,41 @@
 // ProfilePage.jsx
 
-import {Box, Flex, Grid, GridItem, Heading, Text} from "@chakra-ui/react";
+import {Box, Flex, Grid, Heading, Text} from "@chakra-ui/react";
 import SidebarComponent from "@/components/layout/SidebarComponent.jsx";
 import PersonalInfoCard from "@/components/layout/PersonalInfoCard.jsx";
+import InfoCard from "@/components/shared/InfoCard.jsx";
+import {Briefcase} from "lucide-react";
 
 export default function ProfilePage() {
     return (
         <>
             <Flex direction={"row"}>
                 <SidebarComponent/>
-                <Grid templateColumns={{base: "1fr", lg: "3fr 1fr"}} >
+                <Grid width={"100%"} templateColumns={{base: "1fr", lg: "4fr 1fr"}}>
 
-                    <Box bg={"aqua"}>
-                        <PersonalInfoCard/>
+                    <Box bg={"secondary.50"}>
+
+                        <Box ml={["20px", "80px"]} mt={"35px"}>
+                            <Box>
+                                <Heading fontSize={"30px"} fontWeight={"bold"}>My Profile</Heading>
+                                <Text fontSize={"16px"}>Find your personal information and activities rate</Text>
+                            </Box>
+
+                            {/*--- Import Personal Info Card ---*/}
+                            <PersonalInfoCard mt={"35px"}/>
+
+
+                        </Box>
 
                     </Box>
+
                     <Box bg={"black"}>
-                        <Text>Find your personal information and activities rate</Text>
-                    </Box>
 
+                        <InfoCard upperText={"Activities"} text={"6"}
+                                  bg={"primary.200"} icon={Briefcase} iconSize={"45px"}
+                                  upperTextColor={"grey"} textColor={"primary.500"}/>
+
+                    </Box>
 
 
                 </Grid>
